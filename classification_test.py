d48de939cb464bfc0bf167f57b07e2c5e7237a1d
Pedro Faustini <pedro.faustini@ufabc.edu.br>
2018-07-20 00:53:37 -0300

    MFCC só com valores positivos.

--- classification_test.py
+++ classification_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+from math import fabs
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
@@ -12,6 +13,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+from spectrum import get_spectrum
 
 SAMPLE_RATE = 44100
 TRAINING_OUTPUT = 'output_training/'
@@ -31,11 +33,14 @@ def extract_features(audio_filename: str, path: str) -> pd.core.series.Series:
     try:
         label = audio_filename.split('.')[0].split('-')[-1]
 
-        ft1 = librosa.feature.mfcc(data, sr=SAMPLE_RATE, n_mfcc=40)
+        ft1_raw = librosa.feature.mfcc(data, sr=SAMPLE_RATE, n_mfcc=40)
+        
+        ft1 = np.array([list(map(fabs, sublist)) for sublist in ft1_raw]) # Tudo positivo
+
         npstd = np.std(ft1, axis=1)
         npmedian = np.median(ft1, axis=1)
         delta = np.max(ft1) - np.min(ft1)
-        ft1_trunc = np.hstack((npmedian, delta))
+        ft1_trunc = np.hstack((npmedian, npstd, delta))
 
         ft2 = librosa.feature.zero_crossing_rate(y=data)
         ft2_trunc = ft2.size
@@ -47,7 +52,7 @@ def extract_features(audio_filename: str, path: str) -> pd.core.series.Series:
         #plt.tight_layout()
         #plt.show()
         
-        features = pd.Series(np.hstack((ft1_trunc, ft2_trunc, label)))
+        features = pd.Series(np.hstack((ft1_trunc, ft2_trunc, get_spectrum(data), label)))
         return features
     except:
         print('bad file')
@@ -66,40 +71,26 @@ def train() -> tuple:
         X_train_raw.append(d)
         y_train.append(l)
 
-    # Normalise
+    # Normalisar
     std_scale = preprocessing.StandardScaler().fit(X_train_raw) 
     X_train = std_scale.transform(X_train_raw)
     return X_train, y_train, std_scale
 
 
 def test(X_train: np.ndarray, y_train: np.ndarray, std_scale: preprocessing.data.StandardScaler):
-    accuracyNB = 0
     accuracy1NN = 0
-    accuracy3NN = 0
     accuracySVM = 0
     for folder in TEST_AUDIO_CAPTCHA_FOLDERS:
-        correctNB = 0
         correct1NN = 0
-        correct3NN = 0
         correctSVM = 0
         for filename in os.listdir(folder):
             obj = extract_features(filename, folder)
             y_test = obj[obj.size - 1]
             X_test_raw = [obj[0:obj.size - 1]]
-            X_test = std_scale.transform(X_test_raw) # normalise
-            
-            gnb = GaussianNB()
-            y_pred = gnb.fit(X_train, y_train).predict(X_test)
-            if y_pred[0] == y_test:
-                correctNB+=1
+            X_test = std_scale.transform(X_test_raw) # Normalisar
             
             neigh1 = KNeighborsClassifier(n_neighbors=1)
             y_pred = neigh1.fit(X_train, y_train).predict(X_test)
-            if y_pred[0] == y_test:
-                correct1NN+=1
-
-            neigh3 = KNeighborsClassifier(n_neighbors=1)
-            y_pred = neigh3.fit(X_train, y_train).predict(X_test)
             if y_pred[0] == y_test:
                 correct1NN+=1
 
@@ -107,21 +98,14 @@ def test(X_train: np.ndarray, y_train: np.ndarray, std_scale: preprocessing.data
             y_pred = clf.fit(X_train, y_train).predict(X_test)
             if y_pred[0] == y_test:
                 correctSVM+=1
-        
-        if correctNB == 4:
-            accuracyNB+=1
+
         if correct1NN == 4:
             accuracy1NN+=1
-        if correct3NN == 4:
-            accuracy3NN+=1
         if correctSVM == 4:
             accuracySVM+=1
 
     print("Acuracia 1NN = "+str(accuracy1NN / len(TEST_AUDIO_CAPTCHA_FOLDERS)))
-    print("Acuracia 1NN = "+str(accuracy3NN / len(TEST_AUDIO_CAPTCHA_FOLDERS)))
     print("Acuracia SVM = "+str(accuracySVM / len(TEST_AUDIO_CAPTCHA_FOLDERS)))
-    print("Acuracia Naive Bayes = "+str(accuracyNB / len(TEST_AUDIO_CAPTCHA_FOLDERS)))
-
 
 
 def important_features() -> np.ndarray:
@@ -141,4 +125,4 @@ def break_captcha():
 
 if __name__ == "__main__":
     break_captcha()
-    #a=important_features()
+    a=important_features()
