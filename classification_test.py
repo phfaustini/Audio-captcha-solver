a1e7aa0d80882d950846033ae275b11b5f304d73
Matheus Fernandes <mcfernandes1110@gmail.com>
2018-08-17 19:14:27 -0300

    Corrigindo erro na geração de ensemble

--- classification_test.py
+++ classification_test.py
@@ -16,7 +16,6 @@ from sklearn.ensemble import VotingClassifier
 
 from count_peaks import count_peaks
 
-
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
@@ -40,8 +39,6 @@ number_of_folders = len(TEST_AUDIO_CAPTCHA_FOLDERS)
 number_of_characters = len(TEST_AUDIO_FILENAMES)
 
 
-
-
 def make_chain(low, high):
     return (AudioEffectsChain()
         .lowpass(high, 3.0)
@@ -56,18 +53,14 @@ p = make_chain(4000, 6000)
 br = make_chain(6000, 20000)
 
 specs = [sb,b,lm,m,um,p,br]
-
-
 def get_spectrum(audio):
-    """Uso: chame a funcao get_spectrum. 
+    """Uso: chame a funcao get_spectrum.
 
     Ela retorna a intensidade média de cada um dos 7 espectros de frequência.
     Será sempre um valor entre 0 e 1, normalmente mais próximo de 0 e acredito que
     nunca acima de 0,5.
-    """    
+    """
     return [np.mean(np.abs(spectrum(audio))) for spectrum in specs]
-
-
 
 def extract_features(audio_filename: str, path: str) -> pd.core.series.Series:
     """
@@ -80,7 +73,7 @@ def extract_features(audio_filename: str, path: str) -> pd.core.series.Series:
     label = audio_filename.split('.')[0].split('-')[-1]
 
     feature1_raw = librosa.feature.mfcc(data, sr=SAMPLE_RATE, n_mfcc=40)
-    
+
     feature1 = np.array([list(map(fabs, sublist)) for sublist in feature1_raw]) # Tudo positivo
 
     npstd = np.std(feature1, axis=1)
@@ -91,14 +84,12 @@ def extract_features(audio_filename: str, path: str) -> pd.core.series.Series:
     feature2_flat = feature2.size / data.size
 
     feature5_flat = get_spectrum(data)
-    
+
     feature6_flat = count_peaks(data)
-    
+
     features = pd.Series(np.hstack((feature1_flat, feature2_flat,
                                     feature5_flat, feature6_flat, label)))
     return features
- 
-
 
 def train() -> tuple:
     X_train_raw = []
@@ -111,25 +102,32 @@ def train() -> tuple:
         l = obj[obj.size - 1]
         X_train_raw.append(d)
         y_train.append(l)
-    std_scale = preprocessing.StandardScaler().fit(X_train_raw) 
+    std_scale = preprocessing.StandardScaler().fit(X_train_raw)
     X_train = std_scale.transform(X_train_raw)
     return X_train, np.array(y_train), std_scale
 
+
+def create_classifier():
+        clf1 = KNeighborsClassifier(n_neighbors=1)
+        clf2 = RandomForestClassifier(random_state=100)
+        clf3 = SVC(random_state=100)
+        clf4 = LinearDiscriminantAnalysis()
+        clf5 = KNeighborsClassifier(n_neighbors=3)
+
+        #creates ensemble
+        return VotingClassifier(estimators=[('1nn', clf1), ('rf', clf2), ('svm', clf3), ('lda', clf4), ('5nn', clf5)], voting='hard')
 
 def test(X_train: np.ndarray, y_train: np.ndarray, std_scale: preprocessing.data.StandardScaler) -> tuple:
     errados      = {'6':0, '7':0, 'a':0, 'b':0, 'c':0, 'd':0, 'h':0, 'm':0, 'n':0, 'x':0}
     corretos     = {'6':0, '7':0, 'a':0, 'b':0, 'c':0, 'd':0, 'h':0, 'm':0, 'n':0, 'x':0}
     elementos    = {'6':0, '7':0, 'a':0, 'b':0, 'c':0, 'd':0, 'h':0, 'm':0, 'n':0, 'x':0}
-    
+
     accuracyTotal = 0
     total = 0
 
-    clf1 = KNeighborsClassifier(n_neighbors=1)
-    clf2 = RandomForestClassifier(random_state=100)
-    clf3 = SVC()
-    clf4 = LinearDiscriminantAnalysis()
-    clf5 = KNeighborsClassifier(n_neighbors=3)
-    
+    eclf = create_classifier()
+    eclf = eclf.fit(X_train, y_train)
+
     for folder in TEST_AUDIO_CAPTCHA_FOLDERS:
         correct = 0
         for filename in os.listdir(folder):
@@ -137,10 +135,7 @@ def test(X_train: np.ndarray, y_train: np.ndarray, std_scale: preprocessing.data
             y_test = obj[obj.size - 1]
             X_test_raw = [obj[0:obj.size - 1]]
             X_test = std_scale.transform(X_test_raw)
-            elementos[y_test] += 1                
-
-            eclf = VotingClassifier(estimators=[('1nn', clf1), ('rf', clf2), ('svm', clf3), ('lda', clf4), ('5nn', clf5)], voting='hard')
-            eclf = eclf.fit(X_train, y_train)
+            elementos[y_test] += 1
             y_pred = eclf.predict(X_test)
             if y_pred[0] == y_test:
                 correct+=1
@@ -148,7 +143,7 @@ def test(X_train: np.ndarray, y_train: np.ndarray, std_scale: preprocessing.data
                 corretos[y_test] += 1
             else:
                 errados[y_test] += 1
-            
+
         if correct == 4:
             accuracyTotal+=1
 
