de191a400d1bb999d94b801a0b2ecc4d0cf5712d
Pedro Faustini <pedro.faustini@ufabc.edu.br>
2018-08-21 16:27:35 -0300

    Melhorias no report.

--- display_results.py
+++ display_results.py
@@ -2,7 +2,7 @@ from IPython.display import Markdown
 
 
 def resultados_caracteres(title: str, correct: dict, wrong: dict, elements: dict):
-    header = "### RESULTADOS "+title + "\n|Caractere| Acerto        | Erro          |\n"
+    header = "### "+title + "\n|Caractere| Acerto        | Erro          |\n"
     line = "|:--------|---------------:|:---------------|\n"
     lines = []
     for letter in ['6', '7', 'a', 'b', 'c', 'd', 'h', 'm', 'n', 'x']:
@@ -20,11 +20,12 @@ def resultados_caracteres(title: str, correct: dict, wrong: dict, elements: dict
 
 def resultados_acuracia(title: str, info: list):
     print()
-    header = "### RESULTADOS "+title + "\n| Métrica | Taxa  |\n"
+    header = "#### Acurácia "+title + "\n| Métrica | Taxa  |\n"
     line = "|:-:|:-:|\n"
     lines = []
-    for l in info:
-        lines.append("|"+l[0]+"| "+l[1]+"%|")
+    for arr in info:
+        for l in arr:
+            lines.append("|"+l[0]+"| "+l[1]+"%|")
     lines = "\n".join(lines)
     text = header+line+lines
     return (Markdown(text))
