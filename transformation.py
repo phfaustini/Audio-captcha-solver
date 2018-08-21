2e500cccce3c50e894604dad9dd422ca7539c624
Matheus Fernandes <mcfernandes1110@gmail.com>
2018-08-21 15:50:37 -0300

    Adicionando nova feature de contagem de picos

--- transformation.py
+++ transformation.py
@@ -10,8 +10,9 @@ from constants import *
 
 
 def _count_peaks(audio):
-    audio = np.abs(audio)
-    a2 = _moving_average(audio, 4410)
+    _, perc = librosa.effects.hpss(audio)
+    y = np.abs(perc)
+    a2 = _moving_average(y, 441)
     #h = np.mean(a2)-np.std(a2)/2
     r0, _ = find_peaks(a2, width=441, height=0.05)
     return len(r0)
@@ -68,7 +69,7 @@ def extract_features(audio_filename: str, path: str) -> pd.core.series.Series:
 
     feature5_flat = _get_spectrum(data)
 
-    #feature6_flat = _count_peaks(data)
+    feature6_flat = _count_peaks(data)
 
     sc = librosa.feature.spectral_contrast(y=data, sr=SAMPLE_RATE)
 
@@ -78,7 +79,7 @@ def extract_features(audio_filename: str, path: str) -> pd.core.series.Series:
     rms_std = np.std(rms)
 
     features = pd.Series(np.hstack((feature1_flat, feature2_flat,
-                                    feature5_flat, [np.mean(sc), np.std(sc)], 
+                                    feature5_flat, feature6_flat, [np.mean(sc), np.std(sc)], 
                                     rms_median, rms_mean, rms_std, 
                                     label)))
     return features
